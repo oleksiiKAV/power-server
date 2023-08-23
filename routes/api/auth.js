bd15645ee3322e6aede98e42405609913f293c27
oleksiiKAV <oleksii.kav@gmail.com>
2023-08-23 17:29:19 +0300

    change user routers

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,11 +4,11 @@ const { validateBody, authenticate, upload } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 const router = express.Router();
 
-router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
-router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+router.post('/signup', validateBody(schemas.registerSchema), ctrl.register);
+router.post('/signin', validateBody(schemas.loginSchema), ctrl.login);
 router.get('/current', authenticate, ctrl.currentUser);
 router.patch('/current/edit', authenticate, upload.single('avatar'), validateBody(schemas.updateSchema), ctrl.updateUser);
 router.post('/body', authenticate, validateBody(schemas.addBodyDataSchema), ctrl.addBodyData);
-router.post('/logout', authenticate, ctrl.logout);
+router.post('/signout', authenticate, ctrl.logout);
 
 module.exports = router;
