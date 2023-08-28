78da8a232df1331718fed803fed9d43341a74fad
oleksiiKAV <oleksii.kav@gmail.com>
2023-08-28 18:21:04 +0300

    api docs - user path

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./power-pulse-api-docs.json');
+
 const usersRouter = require("./routes/api/auth");
 
 const exercisesRouter = require("./routes/api/exercises");
@@ -14,6 +18,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", usersRouter);
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ctrl = require('../../controllers/users');
-const { validateBody, authenticate, upload } = require('../../middlewares');
+const { validateBody, authenticate, upload} = require('../../middlewares');
 const { schemas } = require('../../models/user');
 const router = express.Router();
 
 router.post('/signup', validateBody(schemas.registerSchema), ctrl.register);
 router.post('/signin', validateBody(schemas.loginSchema), ctrl.login);
 router.get('/current', authenticate, ctrl.currentUser);
-router.patch('/current/edit', authenticate, upload.single('avatar'), validateBody(schemas.updateSchema), ctrl.updateUser);
+router.post('/current/edit', authenticate, upload.single('avatar'), validateBody(schemas.updateSchema), ctrl.updateUser);
+
 router.post('/body', authenticate, validateBody(schemas.addBodyDataSchema), ctrl.addBodyData);
 router.post('/signout', authenticate, ctrl.logout);
 
