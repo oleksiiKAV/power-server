fc22bcbbed6233f41ff6ca1c7f70ef3bb07a5efb
oleksiiKAV <oleksii.kav@gmail.com>
2023-08-29 11:11:37 +0300

    added get general info - dashboard router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./power-pulse-api-docs.json');
 
+const dashRouter = require("./routes/api/dashboard");
 const usersRouter = require("./routes/api/auth");
 
 const exercisesRouter = require("./routes/api/exercises");
@@ -20,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.use("/api/dashboard", dashRouter);
 app.use("/api/users", usersRouter);
 
 app.use("/api/exercises", exercisesRouter);
