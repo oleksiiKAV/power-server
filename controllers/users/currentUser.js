0d2b10cd7e4e33452e1bbbf2813b1b98f3c5690c
oleksiiKAV <oleksii.kav@gmail.com>
2023-09-01 18:29:44 +0300

    update added body and add refresh-token

--- controllers/users/currentUser.js
+++ controllers/users/currentUser.js
@@ -1,6 +1,20 @@
+const {generateToken} = require("../../helpers/generateToken");
+const jwt = require('jsonwebtoken');
+
 const currentUser = async (req, res) => {
   const { _id, name, email, token, avatar, bodyData } = req.user;
+  const tokenData = jwt.decode(token);
+  const currentTime = Math.floor(Date.now() / 1000); 
+  const tokenExpiration = tokenData.exp;
+
+  if (tokenExpiration - currentTime < 300) {
+    const newToken = generateToken(_id);
+    await User.findByIdAndUpdate(_id, { token: newToken });
+    res.json({ _id, name, email, token: newToken, avatar, bodyData });
+  } else {
+   
   res.json({ _id, name, email, token, avatar, bodyData });
+}
 };
 
 module.exports = currentUser;
