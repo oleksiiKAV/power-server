a555906087f4d3dee08a8cd5660ce71c8a91d2d8
oleksiiKAV <oleksii.kav@gmail.com>
2023-09-04 13:08:12 +0300

    fix updateUsers

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -6,7 +6,16 @@ const defaultAvatar = 'http://localhost:3000/noname.png';
 const updateUser = async (req, res) => {
   const { _id } = req.user;
   const { file } = req;
-  const updateUser = { ...req.body, ...(file ? { avatar: file.path } : { avatar: defaultAvatar }) };
+  // const updateUser = { ...req.body, ...(file ? { avatar: file.path } : { avatar: defaultAvatar }) };
+  const updateUser = { ...req.body };
+  
+  if (file) {
+    updateUser.avatar = file.path;
+  }
+  
+  if (!updateUser.avatar) {
+    delete updateUser.avatar;
+  }
   const results = await User.findByIdAndUpdate(_id, updateUser, { new: true }).select('name email avatar bodyData token');
   res.json(results);
 };
